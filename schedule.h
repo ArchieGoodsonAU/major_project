f97da111b46b72b54e3f73657eaef41cdbfa31d5
ArchieGoodsonAU <109931748+ArchieGoodsonAU@users.noreply.github.com>
2022-10-18 21:27:43 +1030

    Update schedule.h

--- schedule.h
+++ schedule.h
@@ -1,10 +1,11 @@
+//defensive programming
 #ifndef SCHEDULE_H
 #define SCHEDULE_H
 class schedule{
     public:
-    int mySchedule[5][3];
-    schedule();
-    void update_schedule(int day, int shift, int updated_avail);
-    void print_schedule();
+    int mySchedule[5][3]; //2D array of all shifts across all days
+    schedule(); //initialiser
+    void update_schedule(int day, int shift, int updated_avail); //to update shift information
+    void print_schedule(); //display shift roster
 };
 #endif
