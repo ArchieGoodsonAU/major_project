ecd58e6b82d4518b819b09077809fdff5d8bd19f
James12345 <jliu3372@gmail.com>
2022-10-18 20:23:36 +1030

    lol

--- manager.h
+++ manager.h
@@ -1,16 +1,16 @@
-#include "fullTimeEmployee.h"
-#include "employee.h"
 #include <iostream>
+#include "employee.h"
+#include "fullTimeEmployee.h"
 #ifndef MANAGER_H
 #define MANAGER_H
-class manager : public fullTimeEmployee{
-    public:
-    int ID;
-    manager(std::string name);
-    manager();
-    std::string get_password();
-    private:
-    std::string password;
+class manager : public fullTimeEmployee {
+ public:
+  int ID;
+  manager(std::string name);
+  manager();
+  std::string get_password();
 
+ private:
+  std::string password;
 };
 #endif
