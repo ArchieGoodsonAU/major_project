f6fbad93c0f4a0a4314f8cf8ab4b666898164885
ArchieGoodsonAU <109931748+ArchieGoodsonAU@users.noreply.github.com>
2022-10-18 20:54:27 +1030

    Update partTimeEmployee.h

--- partTimeEmployee.h
+++ partTimeEmployee.h
@@ -1,23 +1,25 @@
+//include header files and libraries
 #include "employee.h"
 #include "schedule.h"
 #include <iostream>
+
+//defensive programming
 #ifndef PARTTIMEEMPLOYEE_H
 #define PARTTIMEEMPLOYEE_H
+
+
 class partTimeEmployee : public employee{
+    //Employee with changeable max hours
     public:
-        schedule mySchedule;
-        void schedule_unavailabity();
-        int hourly_wage;
-        partTimeEmployee();
-        partTimeEmployee(std::string name, int max_hours, int ID);
-        partTimeEmployee(std::string name, int max_hours, int ID, int hourly_rate);
-        void setData(std::string name, int max_hours, int ID);
-        void setData(std::string name, int max_hours, int ID, int hourly_rate);
-        void call_off(int day, int shift);
+        schedule mySchedule; //Work schedule
+        int hourly_wage; //Hourly pay
+        partTimeEmployee(); //Default initialiser
+        partTimeEmployee(std::string name, int max_hours, int ID); //Key variables initialiser
+        partTimeEmployee(std::string name, int max_hours, int ID, int hourly_rate); //All data initialiser
+        void setData(std::string name, int max_hours, int ID); //Update key information
+        void setData(std::string name, int max_hours, int ID, int hourly_rate); //Update all information
+        void call_off(int day, int shift); //Schedule unavailability
         void call_of(int day, int shift);
-    /*
-    void countAvailability(int availability[]);
-    int* setAvailability(int n, int availability[]);
-    */
+
 };
 #endif
