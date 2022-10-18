43e2a8ac12502a2b39f3f64b3e304554ddbfc4f5
ArchieGoodsonAU <109931748+ArchieGoodsonAU@users.noreply.github.com>
2022-10-18 20:50:47 +1030

    Update partTimeEmployee.cpp

--- partTimeEmployee.cpp
+++ partTimeEmployee.cpp
@@ -1,49 +1,55 @@
+//include headers and libraries
 #include "employee.h"
 #include "partTimeEmployee.h"
 #include "schedule.h"
 #include <iostream>
+
+
 partTimeEmployee::partTimeEmployee(std::string name, int given_max_hours, int ID){
-    //countAvailability(availability);
-    //unavailability = setAvailability(unavailability_count, availability);
-    this->firstName = name;
-    this->max_hours = given_max_hours;
-    this->id = ID;
+    //Initialiser with key information
+
+    this->firstName = name; //For that personal touch
+    this->max_hours = given_max_hours; //Maximum hours to work per week
+    this->id = ID; //Numerical ID
 }
 partTimeEmployee::partTimeEmployee(){
-    this->firstName = "N/A";
-    this->max_hours = -1;
-    this->id = 0;
-    //this->mySchedule = schedule();
+    //Empty/default initialiser
+    this->firstName = "N/A"; //No name given
+    this->max_hours = -1; //To differentiate from employees with no max hours
+    this->id = 0; //Empty numerical ID
+
 }
 partTimeEmployee::partTimeEmployee(std::string name, int given_max_hours, int ID, int hourly_rate){
-    //std::cout << "attempt made";
-    mySchedule = schedule();
-    hourly_wage = hourly_rate;
-    firstName = name;
-    max_hours = given_max_hours;
-    id = ID;
-    //this->mySchedule = schedule();
+    //Full initialiser
+    mySchedule = schedule(); //initialise schedule
+    hourly_wage = hourly_rate; //Hourly pay
+    firstName = name; //For that personal touch
+    max_hours = given_max_hours; //Maximum hours per week
+    id = ID; //Numerical ID
+
 }
 
 void partTimeEmployee::setData(std::string name, int given_max_hours, int ID){
-    //std::cout << "attempt made";
-    hourly_wage = 21;
-    firstName = name;
-    max_hours = given_max_hours;
-    id = ID;
-    //this->mySchedule = schedule();
+    //Update/set key data
+
+    hourly_wage = 21; //Minimum wage (default pay)
+    firstName = name; //For that personal touch
+    max_hours = given_max_hours; //Max weekly hours
+    id = ID; //Numerical ID 
+
 }
 
 void partTimeEmployee::call_off(int day, int shift){
-        mySchedule.update_schedule(day, shift, 1);
-        for(int i = 0; i < 5; i++){
-            for(int j = 0; j < 3; j++){
-                std::cout << j <<  mySchedule.mySchedule[i][j] << " ";
+    //for setting unavailability
+        mySchedule.update_schedule(day, shift, 1); //Update schedule to unworkable
+        for(int i = 0; i < 5; i++){ //loop through days
+            for(int j = 0; j < 3; j++){ //loop through shifts
+                std::cout << j <<  mySchedule.mySchedule[i][j] << " "; //display schedule
             }
-            std::cout << std::endl;
+            std::cout << std::endl; // new line
         }
 }
-void partTimeEmployee::call_of(int day, int shift){
+void partTimeEmployee::call_of(int day, int shift){ //
         mySchedule.update_schedule(day, shift, 1);
         std::cout << shift << std::endl;
         for(int i = 0; i < 5; i++){
@@ -55,23 +61,12 @@ void partTimeEmployee::call_of(int day, int shift){
 }
 
 void partTimeEmployee::setData(std::string name, int given_max_hours, int ID, int hourly_rate){
-    hourly_wage = hourly_rate;
-    firstName = name;
-    max_hours = given_max_hours;
-    id = ID;
-    //this->mySchedule = schedule();
+    //Full data reassignment
+    hourly_wage = hourly_rate; //Set hourly pay
+    firstName = name; //For that personal touch
+    max_hours = given_max_hours; //Max weekly workload
+    id = ID; //Numerical ID
 }
-/*
-void partTimeEmployee::countAvailability(int availability[]){
-    int unavailability_count = 0;
-    for(int i = 0; i < 15; i++){
-        unavailability_count += availability[i];
-    }
-}
-int* partTimeEmployee::setAvailability(int n, int availability[]){
-    unavailability = new int[n];
-    for(int i = 0; i < )
-}
-*/
+
 
 
