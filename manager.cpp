10165e49130ed34db3d3f32fdfea20a4b6298c3d
ArchieGoodsonAU <109931748+ArchieGoodsonAU@users.noreply.github.com>
2022-10-18 20:43:11 +1030

    Update manager.cpp

--- manager.cpp
+++ manager.cpp
@@ -1,26 +1,28 @@
+#include headers and libraries 
 #include "manager.h"
 #include "employee.h"
 #include "roster.h"
 #include "fullTimeEmployee.h"
 #include <iostream>
-manager::manager(std::string name){
-    //countAvailability(availability);
-    //unavailability = setAvailability(unavailability_count, availability);
-    salary = 70000;
-    this->firstName = name;
-    this->max_hours = 45;
-    this->id = 1;
-    this->password = "ArchieRocks";
+
+
+manager::manager(std::string name){ 
+    //Initialiser with set name
+    
+    salary = 70000; //Manager's salary
+    this->firstName = name; //For that personal touch
+    this->max_hours = 45; //Available for all 15 shifts
+    this->id = 1; //Manager's ID
+    this->password = "ArchieRocks"; //Used to gain access to program (My IRL password (Not really!)) 
 }
 manager::manager(){
-    //countAvailability(availability);
-    //unavailability = setAvailability(unavailability_count, availability);
-    salary = 70000;
-    this->firstName = "Marco";
-    this->max_hours = 45;
-    this->id = 1;
-    this->password = "ArchieRocks";
+    //Initialiser with default name
+    salary = 70000; //Manager's salary
+    this->firstName = "Marco"; //My bosses name as a default name
+    this->max_hours = 45; //Available for all 15 shifts
+    this->id = 1; //Manger's ID
+    this->password = "ArchieRocks"; //Used to gain access to program
 }
 std::string manager::get_password(){
-    return password;
+    return password; //As password is private, has to be retrieved through a public function
 }
