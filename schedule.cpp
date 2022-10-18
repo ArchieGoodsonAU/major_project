d24051218498397476a7ef4ac86c8d597942d65e
ArchieGoodsonAU <109931748+ArchieGoodsonAU@users.noreply.github.com>
2022-10-18 21:26:48 +1030

    Update schedule.cpp

--- schedule.cpp
+++ schedule.cpp
@@ -1,22 +1,25 @@
 #include <iostream>
 #include "schedule.h"
+
 schedule::schedule(){
-    for(int day = 0; day < 5; day++){
-        for(int shift = 0; shift < 3; shift++){
-            mySchedule[day][shift] = 1;
+    //default initialiser, initialise all shifts to unavailable/not working
+    for(int day = 0; day < 5; day++){ //loop through each day
+        for(int shift = 0; shift < 3; shift++){ //loop through each shift
+            mySchedule[day][shift] = 1; //assign to 1 (not working)
         }
     }
 }
+
 void schedule::update_schedule(int day, int shift, int updatedAvail){
-    mySchedule[day][shift] = updatedAvail;
-    std::cout << shift << std::endl;
+    mySchedule[day][shift] = updatedAvail; //updated required shift to desired value
 }
 void schedule::print_schedule(){
-    for(int day = 0; day < 5; day++){
-        std::cout << "Day " << day + 1 << ": ";
-        for(int shift = 0; shift < 3; shift++){
-            std::cout << mySchedule[day][shift] << " ";
+    //this function prints the schedule in a nice format to the console
+    for(int day = 0; day < 5; day++){ //loop through each day
+        std::cout << "Day " << day + 1 << ": "; //show day number
+        for(int shift = 0; shift < 3; shift++){ // loop through each shift
+            std::cout << mySchedule[day][shift] << " "; //display each shift
         }
-        std::cout << std::endl;
+        std::cout << std::endl; // new line
     }
 }
